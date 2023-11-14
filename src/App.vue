1c2ad076cd8081c0f58a4962273df24cd7542b7a
rakibsarowar <rakibsarowar@gmail.com>
2023-11-14 21:39:31 +0600

    working on setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+  import { ref } from "vue";
+  const product = ref('socks')
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div class="nav-bar"></div>
+  <div>
+    <h1>{{ product }}</h1>
+  </div>
+</template>
+
+
